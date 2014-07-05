fcfe00426769b1c67d2732cd1250dcc753db4497
Arnel Estanislao <arnel.estanislao@datakin.io>
2014-07-05 13:34:54 +0800

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,63 @@
+
+$(document).ready(function () {
+	
+	/***************************************
+	** the look layout carousel slider
+	***************************************/
+	
+	$('.layoutslider').bxSlider();
+	
+	
+	/***************************************
+	** the look specifications
+	***************************************/
+	
+	$('ul.specifications li').click(function(){
+		var current_specs = $(this).attr('rel');
+		$('ul.specifications li').removeClass('selected');
+		$(this).addClass('selected');
+		$('.specs-content').hide();
+		$(current_specs).show();
+	});
+	
+	
+	/***************************************
+	** the area map links
+	***************************************/
+	
+	$('.map-links div div img').click(function(){
+		if (!($(this).hasClass('active'))) {
+			var current_map = $(this).attr('rel');
+			$('.map-links img').removeClass('active');
+			$(this).addClass('active');
+			$('.map-container img').attr('src','images/'+current_map+'.png');
+		}
+	});
+	
+	
+	/***************************************
+	** Lightbox
+	***************************************/
+	
+	$('.bt-leasingnow').fancybox({
+		padding: 0,
+		margin: 0,
+		width: 470,
+		height: 470
+	});
+	
+	$('.footer-links').fancybox({
+		padding: 0,
+		margin: 0,
+		width: 700,
+		height: 700
+	});
+	
+	$('.bt-leasingnow').click(function(){
+		setTimeout(function(){ $('.fancybox-wrap, .fancybox-skin').addClass('leasingnow-wrapper'); }, 100);
+	});
+	
+	$('.footer-links').click(function(){
+		setTimeout(function(){ $('.fancybox-wrap, .fancybox-skin').addClass('footerlinks-wrapper'); }, 100);
+	});
+});
